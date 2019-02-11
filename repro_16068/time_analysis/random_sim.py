292f60a063a229e5a2f62f8bab7ee769795f49a1
Pavan Hebbar <rpavanhebbar1996@gmail.com>
2019-02-11 11:52:11 -0700

    Paper resubmitted

--- repro_16068/time_analysis/random_sim.py
+++ repro_16068/time_analysis/random_sim.py
@@ -1,6 +1,7 @@
 """Python program to simulate rendom realizations and get a dist of chi^2"""
 
 import sys
+import copy
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
@@ -69,26 +70,34 @@ def fit_funcs(xdata, ydata):
     # Fitting a constant function
     const_val, const_err = curve_fit(func_const, xdata, ydata,
                                      p0=np.mean(ydata),
-                                     sigma=1+(ydata+0.75)**0.5)
+                                     sigma=1+(ydata+0.75)**0.5,
+                                     absolute_sigma=True)
     chi_const = get_chi(const_val, func_const, xdata, ydata)
 
     # Fitting a sine function
     sin_min = [0., 3*xlen, 0., 0.]
     sin_err = [0., 0., 0., 0.]
     chi_sin = get_chi(sin_min, func_sine, xdata, ydata)
-    lboun = [0., binsize*2, np.min(xdata), 0.]
-    uboun = [np.max(ydata), 2.*xlen, np.max(xdata), np.max(ydata)]
+    lboun = [0., binsize*2, -1.0*np.inf, 0.]
+    uboun = [np.max(ydata), 2.*xlen, np.inf, np.max(ydata)]
     test_periods = xlen*1.0/np.arange(1.0, 1.0*int(len(xdata)/2.+0.5), 1.)
+    test_amps = [0., 1., 2., 3., 4., 5.]
     for period in test_periods:
-        sin_val, sin_e = curve_fit(func_sine, xdata, ydata,
-                                   p0=[0., period, np.min(xdata),
-                                       np.mean(ydata)], bounds=(lboun, uboun),
-                                   sigma=1+(ydata+0.75)**0.5)
-        chi_temp = get_chi(sin_val, func_sine, xdata, ydata)
-        if chi_temp < chi_sin:
-            chi_sin = chi_temp
-            sin_min = sin_val.copy()
-            sin_err = sin_e.copy()
+        for amp in test_amps:
+            try:
+                sin_val, sin_e = curve_fit(
+                    func_sine, xdata, ydata, p0=[amp, period, np.min(xdata),
+                                                 np.mean(ydata)],
+                    bounds=(lboun, uboun), sigma=1+(ydata+0.75)**0.5,
+                    absolute_sigma=True, maxfev=10000000)
+            except ValueError:
+                sin_val = copy.copy(sin_min)
+                sin_err = copy.copy(sin_err)
+            chi_temp = get_chi(sin_val, func_sine, xdata, ydata)
+            if chi_temp < chi_sin:
+                chi_sin = chi_temp
+                sin_min = sin_val.copy()
+                sin_err = sin_e.copy()
     sin_err = np.sqrt(np.diag(sin_err))
     return const_val, const_err, chi_const, sin_min, sin_err, chi_sin
 
@@ -116,12 +125,12 @@ def monte_carlo_step(opt=1, optparam=[]):
     elif opt == 2:
         if not len(optparam) == 2:
             raise KeyError('optparam = [xvals, rate] if opt = 2')
-        xvals = optparam[1].copy()
-        yvals = np.random.poisson(optparam[2], len(xvals))
+        xvals = optparam[0].copy()
+        yvals = np.random.poisson(optparam[1], len(xvals))
     else:
         raise ValueError('opt can only take values 1 or 2')
     func_fitting = fit_funcs(xvals, yvals)
-    return func_fitting
+    return func_fitting, xvals, yvals
 
 
 def monte_carlo(ntimes, opt=1, optparam=[]):
@@ -134,11 +143,13 @@ def monte_carlo(ntimes, opt=1, optparam=[]):
     const_params = np.zeros((ntimes, 3))
     sin_params = np.zeros((ntimes, 9))
     for i in range(ntimes):
-        func_fits = monte_carlo_step(opt, optparam)
+        func_fits = monte_carlo_step(opt, optparam)[0]
         const_params[i, :] = func_fits[:3]
         sin_params[i, [0, 2, 4, 6]] = func_fits[3]
         sin_params[i, [1, 3, 5, 7]] = func_fits[4]
         sin_params[i, 8] = func_fits[5]
+        if i % 10 == 0:
+            print(i)
     return const_params, sin_params
 
 
@@ -151,7 +162,7 @@ def plotfig(name, xlabel, ylabel, xdatas, ydatas, legends=[]):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     for i, xdata in enumerate(xdatas):
-        plt.plot(xdata, ydatas[i], label=legends[i])
+        plt.step(xdata, ydatas[i], label=legends[i], where='mid')
     if not legends == legends_null:
         plt.legend()
     plt.savefig(name)
@@ -159,7 +170,7 @@ def plotfig(name, xlabel, ylabel, xdatas, ydatas, legends=[]):
 
 
 def get_stats(const_params, sin_params):
-    """Return information on the constant the sine fit params
+    """Return information on the constant and the sine fit params
 
     Inputs:
     const_params - Constant value, constant error and chi
@@ -174,7 +185,7 @@ def get_stats(const_params, sin_params):
     hist_chi_const, chi_bins = np.histogram(const_params[:, -1], bins=chi_bins)
     hist_chi_sin, chi_bins = np.histogram(sin_params[:, -1], bins=chi_bins)
     chi_vals = 0.5*(chi_bins[:-1] + chi_bins[1:])
-    plotfig('chi_vals.png', r'$\chi$', r'$f$', [chi_vals, chi_vals],
+    plotfig('chi_vals_2.png', r'$\chi$', r'$f$', [chi_vals, chi_vals],
             [hist_chi_const*1.0/len(sin_params),
              hist_chi_sin*1.0/len(sin_params)], ['Constant fit', 'Sine fit'])
 
@@ -186,7 +197,7 @@ def get_stats(const_params, sin_params):
     his_aic_cons, aic_bins = np.histogram(const_params[:, -1]+2, bins=aic_bins)
     hist_aic_sin, aic_bins = np.histogram(sin_params[:, -1]+8, bins=aic_bins)
     aic_vals = 0.5*(aic_bins[:-1] + aic_bins[1:])
-    plotfig('aic_vals.png', 'AIC value', r'$f$', [aic_vals, aic_vals],
+    plotfig('aic_vals_2.png', 'AIC value', r'$f$', [aic_vals, aic_vals],
             [his_aic_cons*1.0/len(sin_params),
              hist_aic_sin*1.0/len(sin_params)], ['Constant fit', 'Sine fit'])
 
@@ -194,19 +205,30 @@ def get_stats(const_params, sin_params):
     conf = sin_params[:, 0]*1.0/sin_params[:, 1]
     hist_conf, conf_bins = np.histogram(conf, bins=11)
     conf_vals = 0.5*(conf_bins[1:] + conf_bins[:-1])
-    plotfig('amp_sin.png', 'Amplitude', r'$f$', [conf_vals],
+    plotfig('amp_sin_2.png', 'Amp/Amp_err', r'$f$', [conf_vals],
             [hist_conf/len(sin_params)])
 
 
-def main(args):
+def main(opt, ntimes):
     """Run the program"""
-    opt, ntimes = args
     opt = int(opt)
     ntimes = int(ntimes)
-    xlim = [0, 162226.01292]
-    length = 179
-    binsize = 5000
-    const_params, sin_params = monte_carlo(
-        ntimes, opt, [xlim[0], xlim[1], length, binsize])
-    np.savetxt('const_params_'+str(opt)+'.txt', const_params)
-    np.savetxt('sin_params_'+str(opt)+'.txt', sin_params)
+    if opt == 1:
+        xlim = [0, 162226.01292]
+        length = 179
+        binsize = 5000
+        const_params, sin_params = monte_carlo(
+                ntimes, 1, [xlim[0], xlim[1], length, binsize])
+    elif opt == 2:
+        xvals = np.arange(0, 162226.01292+5000, 5000)
+        meancounts = 5.659421112195175
+        const_params, sin_params = monte_carlo(
+                ntimes, 2, [xvals, meancounts])
+    np.savetxt('const_params_'+str(opt+ntimes)+'.txt', const_params)
+    np.savetxt('sin_params_'+str(opt+ntimes)+'.txt', sin_params)
+    get_stats(const_params, sin_params)
+
+
+if __name__ == '__main__':
+    print(len(sys.argv))
+    main(sys.argv[1], sys.argv[2])
